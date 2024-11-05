3dabb8be11f3c7527b105bb728aa3a4780828aa8
AugustoMacri <augusto.macri@hotmail.com>
2024-11-05 16:52:32 -0300

    feat: impressao de infos do dataset

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
-
+import plotly.express as px
+import pandas as pd
 
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
@@ -26,14 +27,37 @@ except Exception as e:
 
 if st.session_state['authentication_status']:
     authenticator.logout()
-    
+
     st.write(f'Welcome *{st.session_state["name"]}*')
     x = st.session_state['roles']
     if x is not None:
         if 'admin' in x:
             st.write('Ola Admin')
 
-    st.title('Some content')
+            # plotando gráfico simples
+            # df = px.data.gapminder().query("year == 2007")
+            # fig = px.scatter(df, x="gdpPercap", y="lifeExp", size="pop", color="continent",
+            # hover_name="country", log_x=True, size_max=60)
+            # st.plotly_chart(fig)
+
+        try:
+            # carregando o csv do banco de dados
+            df = pd.read_csv(
+                'data/BASE_UTILIZACAO_PHARMA_E_2017_2021.csv',
+                encoding='ISO-8859-1',
+                sep=';',
+                names=['Comp_Atendimento', 'Data Atendimento', 'NR_ASSOCIADO', 'CID', 'Descrição do CID', 'Grupo Serviço', 'Sub Grupo Serviço',
+                       'Cód. Serviço', 'Descrição do Serviço', 'COD_PRESTADOR', 'Sgl_UF_Prestador', 'Especialidade', 'QTD', 'Custo'],
+                nrows=10000
+            )
+
+            # Exibindo algumas informações do dataset
+            st.write('Informações do dataset')
+            st.dataframe(df)
+
+        except Exception as e:
+            st.error(f"Erro ao tentar abrir o csv: {e}")
+
 elif st.session_state['authentication_status'] is False:
     st.error('Username/password is incorrect')
 elif st.session_state['authentication_status'] is None:
